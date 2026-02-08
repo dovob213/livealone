0e8fd48fda41ce7b854670c376006f6df399587f
chanyoung <chanyoungsoftware@gmail.com>
2026-02-08 12:35:00 +0900

    init

---
+++ src/test/java/com/springboot/livealone/LiveAloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.livealone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiveAloneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
